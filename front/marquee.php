b53c06e5d52aa08f31ad4603dacca03b2ec760b3
mack <macklun@ms7.hinet.net>
2019-12-01 19:58:56 +0800

    步驟十三：完成標題圖片/動態文字前台功能

--- front/marquee.php
+++ front/marquee.php
@@ -1,2 +1,12 @@
 <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-  </marquee>
+<?php
+//取出所有顯示設為1的資料
+$rows=all("ad",["sh"=>1]);
+
+//用迴圈來顯示資料，並在每個字串後加上一些空白做為分隔
+foreach($rows as $r){
+  echo $r['text'] . "&nbsp;&nbsp;&nbsp;&nbsp;";
+}
+?>
+
+</marquee>
