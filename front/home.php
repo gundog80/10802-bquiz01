ef852e3cdd51db54b84dacbabb2b4241c8ac5a22
mack <macklun@ms7.hinet.net>
2019-12-01 20:00:57 +0800

    步驟十四：完成前台動畫圖片輪播及最新消息顯示

--- front/home.php
+++ front/home.php
@@ -3,32 +3,85 @@
   <?php include "marquee.php";?>
   <div style="height:32px; display:block;"></div>
   <!--正中央-->
-  <script>
-    var lin = new Array();
-    var now = 0;
-    if (lin.length > 1) {
-      setInterval("ww()", 3000);
-      now = 1;
-    }
 
-    function ww() {
-      $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
-      //$("#mwww").attr("src",lin[now])
-      now++;
-      if (now >= lin.length)
-        now = 0;
-    }
-  </script>
   <div style="width:100%; padding:2px; height:290px;">
     <div id="mwww" loop="true" style="width:100%; height:100%;">
       <div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
     </div>
   </div>
+  <!--把動畫的JS搬到動畫的區塊後面-->
+  <script>
+    var lin = new Array();
+        //插入PHP程式碼，將可顯示的動畫檔案名放在陣列中
+        lin =[<?php
+                $mv=all("mvim",["sh"=>1]);
+                foreach($mv as $m){
+                  $tmp[]=$m['file'];
+                }
+                
+                echo "'" . implode("','",$tmp) . "'";
+
+              ;?>];
+    var now = 0;
+
+    //先執行一次ww()函式來讓第一張動畫放在畫面中
+    ww();
+
+    //判斷如果lin陣列中有素就執行動畫輪播
+    if (lin.length > 1) {
+
+      //設定為三秒後執行ww()函式
+      setInterval("ww()", 3000);
+
+      //將now變數改為1
+      now = 1;
+    }
+
+    function ww() {
+
+      //在#mwww區塊中放入動畫顯示的HTML碼，並將陣列中的檔名依照now的值來取出
+      $("#mwww").html("<embed loop=true src='./img/" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+      //$("#mwww").attr("src",lin[now])
+
+      //遞增now的值
+      now++;
+
+      //如果now的值超過lin陣列的個數，則重設now為0
+      if (now >= lin.length)
+        now = 0;
+    }
+  </script>
   <div
     style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
     <span class="t botli">最新消息區
+    <?php
+
+      //利用nums()函式來計算最新消息筆數
+      $chk=nums("news",["sh"=>1]);
+
+      //如果筆數大於5筆，則輸出more的文字連結，直接使用行內樣式來控制<a>的顯示位置
+      if($chk>5){
+        echo "<a href='index.php?do=news' style='text-decoration:none;float:right;right:10px'>more...</a>";
+      }
+
+    ?>
     </span>
     <ul class="ssaa" style="list-style-type:decimal;">
+    <?php
+
+      //取出設定為顯示的前五筆資料
+      $rows=all("news",["sh"=>1]," limit 5");
+
+      //利用迴圈把資料印出來
+      foreach($rows as $r){
+        echo "<li>";
+        echo mb_substr($r['text'],0,25,"utf8");  //用mb_substr()來截取字串
+        
+        //另外輸出一個子元素到li下面，將完整的消息內容放在子元素中，這個子元素要設為不顯示
+        echo "<div class='all' style='display:none'>".$r['text']."</div>";
+        echo "</li>";
+      }
+    ?>
     </ul>
     <div id="altt"
       style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
--- front/news.php
+++ front/news.php
@@ -3,9 +3,59 @@
   <?php include "marquee.php";?>
   <div style="height:32px; display:block;"></div>
   <!--正中央-->
+  <h3 class="cent">更多最新消息顯示區</h3>
+  <hr>
+  <?php
+      $useTable="news";
+
+      // 先取得資料表中的總筆數(要注意是否有條件限制，比如顯示不顯示)
+      $sum=nums($useTable);
+
+      // 決定每個頁面的筆數
+      $div=5;
+
+      // 計算總頁數(無條件進位法)
+      $pages=ceil($sum/$div);
+
+      // 採用網址參數的方式來取得當前頁，預設為第一頁
+      $p=(!empty($_GET['p']))?$_GET['p']:1;
+
+      // 計算資料的開始筆數((當前頁-1)*每頁筆數)
+      $start=($p-1)*$div;
+  ?>
+  <ol class="ssaa" style="list-style-type:decimal;" start="<?=($start+1);?>">
+    <?php
+
+      //取出設定為顯示的前五筆資料
+      $rows=all("news",["sh"=>1]," limit $start,$div");
+
+      //利用迴圈把資料印出來
+      foreach($rows as $r){
+        echo "<li class='sswww'>";
+        echo mb_substr($r['text'],0,25,"utf8");  //用mb_substr()來截取字串
+        
+        //另外輸出一個子元素到li下面，將完整的消息內容放在子元素中，這個子元素要設為不顯示
+        echo "<div class='all' style='display:none'>".$r['text']."</div>";
+        echo "</li>";
+      }
+    ?>
+    </ol>
   <div style="text-align:center;">
-    <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-    <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
+  <?php
+
+    if(($p-1)>0){
+      echo "<a href='index.php?do=$useTable&p=".($p-1)."' style='text-decoration:none'> < </a>";
+    }
+
+    for($i=1;$i<=$pages;$i++){
+      $fontSize=($i==$p)?"24px":"16px";
+      echo "<a href='index.php?do=$useTable&p=$i' style='font-size:$fontSize;text-decoration:none'> ".$i." </a>";
+    }
+
+    if(($p+1)<=$pages){
+      echo "<a href='index.php?do=$useTable&p=".($p+1)."' style='text-decoration:none'> > </a>";
+    }
+  ?>
   </div>
 </div>
 <div id="alt"
@@ -14,7 +64,7 @@
 <script>
   $(".sswww").hover(
     function () {
-      $("#alt").html("" + $(this).children(".all").html() + "").css({
+      $("#alt").html("<pre>" + $(this).children(".all").html() + "</pre>").css({
         "top": $(this).offset().top - 50
       })
       $("#alt").show()
